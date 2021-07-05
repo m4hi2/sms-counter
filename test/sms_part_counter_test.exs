b2490d130a7c106e61ab8b9f2594d87d3ea15b01
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-07-05 21:58:55 +0600

    test: character count for both ascii and unicode chars

--- test/sms_part_counter_test.exs
+++ test/sms_part_counter_test.exs
@@ -2,7 +2,24 @@ defmodule SmsPartCounterTest do
   use ExUnit.Case
   doctest SmsPartCounter
 
-  test "greets the world" do
-    assert SmsPartCounter.hello() == :world
+  describe "can count how many characters are in a message" do
+    test "can count english alphabet" do
+      assert SmsPartCounter.count("abc") == 3
+      assert SmsPartCounter.count("asdf") == 4
+      assert SmsPartCounter.count("") == 0
+    end
+
+    test "can count unicode characters" do
+      assert SmsPartCounter.count("কখগ") == 3
+      assert SmsPartCounter.count("আমি") == 3
+      assert SmsPartCounter.count("") == 0
+    end
+
+    test "can count string with space in it" do
+      assert SmsPartCounter.count("abc acb") == 7
+      assert SmsPartCounter.count("asdf asdf") == 9
+      assert SmsPartCounter.count(" ") == 1
+      assert SmsPartCounter.count("আমি তুমি") == 8
+    end
   end
 end
