d385cab9f90888fa636917aff0340ee8212a0185
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-07-05 22:20:34 +0600

    feat: count/1 counts the chars in a string

--- lib/sms_part_counter.ex
+++ lib/sms_part_counter.ex
@@ -3,4 +3,18 @@ defmodule SmsPartCounter do
   Module for detecting which encoding is being used and the character count of SMS text.
   """
 
+  @doc """
+  Counts the characters in a string.
+
+  ## Examples
+    iex> SmsPartCounter.count("Hello")
+    5
+    iex> SmsPartCounter.count("আম")
+    2
+  """
+  @spec count(binary) :: integer()
+  def count(str) when is_binary(str) do
+    String.Unicode.codepoints(str)
+    |> Enum.count()
+  end
 end
