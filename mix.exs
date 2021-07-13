0e832c38126bb98fcd42b57f20fd98910a3947dc
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-07-13 20:16:09 +0600

    chore: bumped version number for use in other apps

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SmsPartCounter.MixProject do
   def project do
     [
       app: :sms_part_counter,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
